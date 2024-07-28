b549d381ada22e5cd504c6d544e05987836ea6f4
Oleksandr Shevtsov <oshevtsov@bellum.ai>
2024-07-28 23:31:18 +0300

    chore: find damn source of origin domain for invite redirect

--- src/app/api/teams/[teamId]/invite/[inviteToken]/route.ts
+++ src/app/api/teams/[teamId]/invite/[inviteToken]/route.ts
@@ -23,6 +23,12 @@ export async function GET(
         console.log(`GEt INVITE`);
         console.log(`req.url=${req.url}`);
         console.log(`req.nextUrl.href=${req.nextUrl.href}`);
+        console.log(`req.nextUrl.host=${req.nextUrl.host}`);
+        console.log(`req.nextUrl.hostname=${req.nextUrl.hostname}`);
+        console.log(`req.headers.get('host')=${req.headers.get("host")}`);
+        console.log(`req.referrer=${req.referrer}`)
+        console.log(`req.nextUrl.basePath=${req.nextUrl.basePath}`)
+        console.log(`req.nextUrl.origin=${req.nextUrl.origin}`)
         redirect("/api/auth/signin?callbackUrl=" + req.url);
     }
 
